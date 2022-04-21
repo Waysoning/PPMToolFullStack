aa33e20de63f6ababea4c699992ef8b2fa9506e7
wayson <wayson1225@outlook.com>
2022-04-21 01:19:28 -0700

    object and repository

---
+++ PPMTool-SpringBoot-Server/src/main/java/io/waysoning/ppmtool/domain/Project.java
@@ -0,0 +1,103 @@
+package io.waysoning.ppmtool.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * @author wayson
+ */
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date startDate;
+    private Date endDate;
+
+    private Date creteDate;
+    private Date updateDate;
+
+    @PrePersist
+    protected void onCreate() {
+        creteDate = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updateDate = new Date();
+    }
+
+    public Project() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public String getProjectIdentifier() {
+        return projectIdentifier;
+    }
+
+    public void setProjectIdentifier(String projectIdentifier) {
+        this.projectIdentifier = projectIdentifier;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public Date getCreteDate() {
+        return creteDate;
+    }
+
+    public void setCreteDate(Date creteDate) {
+        this.creteDate = creteDate;
+    }
+
+    public Date getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(Date updateDate) {
+        this.updateDate = updateDate;
+    }
+
+
+}
