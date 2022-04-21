f429177e36faed3885cd143e20fd84ed2f944fe4
wayson <wayson1225@outlook.com>
2022-04-21 00:50:42 -0700

    first commit

---
+++ PPMTool-SpringBoot-Server/src/main/java/io/waysoning/ppmtool/PpmToolSpringBootServerApplication.java
@@ -0,0 +1,13 @@
+package io.waysoning.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmToolSpringBootServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmToolSpringBootServerApplication.class, args);
+    }
+
+}
---
+++ PPMTool-SpringBoot-Server/src/test/java/io/waysoning/ppmtool/PpmToolSpringBootServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.waysoning.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmToolSpringBootServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
