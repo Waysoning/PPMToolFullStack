0ee7e99294d741adf5cb5c9f390a2898000266a9
wayson <wayson1225@outlook.com>
2022-04-27 01:14:38 -0700

    service and controller

---
+++ PPMTool-SpringBoot-Server/src/main/java/io/waysoning/ppmtool/web/ProjectController.java
@@ -0,0 +1,25 @@
+package io.waysoning.ppmtool.web;
+
+import io.waysoning.ppmtool.domain.Project;
+import io.waysoning.ppmtool.services.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/project")
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @PostMapping("")
+    public ResponseEntity<Project> createNewProject(@RequestBody Project project) {
+        Project project1 = projectService.saveOrUpdateProject(project);
+        return new ResponseEntity<>(project, HttpStatus.CREATED);
+    }
+}
