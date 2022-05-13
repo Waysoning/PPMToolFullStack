af7f35c78b30dde5db3f37c03537abeca21f44cb
wayson <wayson1225@outlook.com>
2022-05-12 22:47:25 -0700

    persist project task

---
+++ PPMTool-SpringBoot-Server/src/main/java/io/waysoning/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,43 @@
+package io.waysoning.ppmtool.services;
+
+import io.waysoning.ppmtool.domain.Backlog;
+import io.waysoning.ppmtool.domain.ProjectTask;
+import io.waysoning.ppmtool.repositories.BacklogRepository;
+import io.waysoning.ppmtool.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    public static final Integer DEFAULT_PROJECT_TASK_PRIORITY = 3;
+    public static final String DEFAULT_PROJECT_TASK_STATUS = "TO_DO";
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    public ProjectTask addProjectTask(String projectIdentifier ,ProjectTask projectTask) {
+
+        // PTs to be added to a specific project, project != null, Backlog exists
+        // set the BL to the project
+
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        projectTask.setBacklog(backlog);
+        backlog.setPTSequence(backlog.getPTSequence() + 1);
+        projectTask.setProjectSequence(projectIdentifier+"-"+backlog.getPTSequence());
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+        if(projectTask.getPriority() == null || projectTask.getPriority() == 0) {
+            projectTask.setPriority(DEFAULT_PROJECT_TASK_PRIORITY);
+        }
+
+        if(projectTask.getStatus() == null || "".equals(projectTask.getStatus())) {
+            projectTask.setStatus(DEFAULT_PROJECT_TASK_STATUS);
+        }
+
+        return projectTaskRepository.save(projectTask);
+    }
+}
