30b4121bd3ca1684e086465a20aeda17d820a9b3
wayson <wayson1225@outlook.com>
2022-05-12 21:04:42 -0700

    entity relationships: Backlog-ProjectTask

--- PPMTool-SpringBoot-Server/src/main/java/io/waysoning/ppmtool/domain/ProjectTask.java
+++ PPMTool-SpringBoot-Server/src/main/java/io/waysoning/ppmtool/domain/ProjectTask.java
@@ -1,5 +1,7 @@
 package io.waysoning.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
@@ -18,6 +20,11 @@ public class ProjectTask {
     private String status;
     private Integer priority;
     private Date dueDate;
+    //ManyToOne with Backlog
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "backlog_id", updatable = false, nullable = false)
+    @JsonIgnore
+    private Backlog backlog;
 
     /**
      * ManyToOne with Backlog
@@ -109,6 +116,14 @@ public class ProjectTask {
 
     public void setUpdatedAt(Date updatedAt) {
         this.updatedAt = updatedAt;
+    }
+
+    public Backlog getBacklog() {
+        return backlog;
+    }
+
+    public void setBacklog(Backlog backlog) {
+        this.backlog = backlog;
     }
 
     @Override
