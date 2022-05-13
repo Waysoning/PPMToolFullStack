1cc22d2949a0b8a80b30d846a2457593e6ce964b
wayson <wayson1225@outlook.com>
2022-05-12 20:29:04 -0700

    Backlog and ProjectTask entities

---
+++ PPMTool-SpringBoot-Server/src/main/java/io/waysoning/ppmtool/repositories/ProjectTaskRepository.java
@@ -0,0 +1,9 @@
+package io.waysoning.ppmtool.repositories;
+
+import io.waysoning.ppmtool.domain.ProjectTask;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+}
