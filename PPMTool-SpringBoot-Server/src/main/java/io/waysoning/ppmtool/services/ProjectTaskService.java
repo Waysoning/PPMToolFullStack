498c002d0166a97a23a0df55672cd336afc0427c
wayson <wayson1225@outlook.com>
2022-05-13 00:31:44 -0700

    handle project not found exception

--- PPMTool-SpringBoot-Server/src/main/java/io/waysoning/ppmtool/services/ProjectTaskService.java
+++ PPMTool-SpringBoot-Server/src/main/java/io/waysoning/ppmtool/services/ProjectTaskService.java
@@ -1,8 +1,11 @@
 package io.waysoning.ppmtool.services;
 
 import io.waysoning.ppmtool.domain.Backlog;
+import io.waysoning.ppmtool.domain.Project;
 import io.waysoning.ppmtool.domain.ProjectTask;
+import io.waysoning.ppmtool.exceptions.ProjectNotFoundException;
 import io.waysoning.ppmtool.repositories.BacklogRepository;
+import io.waysoning.ppmtool.repositories.ProjectRepository;
 import io.waysoning.ppmtool.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,29 +24,43 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
+    @Autowired
+    private ProjectRepository projectRepository;
+
     public ProjectTask addProjectTask(String projectIdentifier ,ProjectTask projectTask) {
 
-        // PTs to be added to a specific project, project != null, Backlog exists
-        // set the BL to the project
+        try{
 
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
-        projectTask.setBacklog(backlog);
-        backlog.setPTSequence(backlog.getPTSequence() + 1);
-        projectTask.setProjectSequence(projectIdentifier+"-"+backlog.getPTSequence());
-        projectTask.setProjectIdentifier(projectIdentifier);
+            // PTs to be added to a specific project, project != null, Backlog exists
+            // set the BL to the project
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+            projectTask.setBacklog(backlog);
+            backlog.setPTSequence(backlog.getPTSequence() + 1);
+            projectTask.setProjectSequence(projectIdentifier+"-"+backlog.getPTSequence());
+            projectTask.setProjectIdentifier(projectIdentifier);
 
-        if(projectTask.getPriority() == null || projectTask.getPriority() == 0) {
-            projectTask.setPriority(DEFAULT_PROJECT_TASK_PRIORITY);
+            if(projectTask.getPriority() == null || projectTask.getPriority() == 0) {
+                projectTask.setPriority(DEFAULT_PROJECT_TASK_PRIORITY);
+            }
+
+            if(projectTask.getStatus() == null || "".equals(projectTask.getStatus())) {
+                projectTask.setStatus(DEFAULT_PROJECT_TASK_STATUS);
+            }
+
+            return projectTaskRepository.save(projectTask);
+        } catch (Exception e) {
+            throw new ProjectNotFoundException("Project ID '" + projectIdentifier + "' not found");
         }
 
-        if(projectTask.getStatus() == null || "".equals(projectTask.getStatus())) {
-            projectTask.setStatus(DEFAULT_PROJECT_TASK_STATUS);
-        }
-
-        return projectTaskRepository.save(projectTask);
     }
 
     public Iterable<ProjectTask> findBacklogById(String backlogId) {
+
+        Project project = projectRepository.findByProjectIdentifier(backlogId);
+        if(project == null) {
+            throw new ProjectNotFoundException("Project ID '" + backlogId + "' not found");
+        }
+
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(backlogId);
     }
 }
