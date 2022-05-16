fcd6aafbadb4eaed17aae52ebd27837dbd42e261
wayson <wayson1225@outlook.com>
2022-05-15 17:19:11 -0700

    ProjectBoard Algorithm

--- ppmtool-react-client/src/components/ProjectBoard/ProjectBoard.js
+++ ppmtool-react-client/src/components/ProjectBoard/ProjectBoard.js
@@ -7,15 +7,53 @@ import { getBacklog } from '../../actions/backlogActions';
 
 class ProjectBoard extends Component {
   // constructor to hanle the errors
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: {},
+    };
+  }
 
   componentDidMount() {
     const { id } = this.props.match.params;
     this.props.getBacklog(id);
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
   render() {
     const { id } = this.props.match.params;
     const { project_tasks } = this.props.backlog;
+    const { errors } = this.state;
+
+    let boardContent;
+
+    const boardAlgorithm = (errors, project_tasks) => {
+      if (project_tasks.length < 1) {
+        if (errors.projectNotFound) {
+          return (
+            <div className="alert alert-danger text-center" role="alert">
+              {errors.projectNotFound}
+            </div>
+          );
+        } else {
+          return (
+            <div className="alert alert-info text-center" role="alert">
+              No Project Tasks on this board
+            </div>
+          );
+        }
+      } else {
+        return <Backlog project_tasks={project_tasks} />;
+      }
+    };
+
+    boardContent = boardAlgorithm(errors, project_tasks);
+
     return (
       <div className="container">
         <Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
@@ -23,7 +61,7 @@ class ProjectBoard extends Component {
         </Link>
         <br />
         <hr />
-        <Backlog project_tasks={project_tasks} />
+        {boardContent}
       </div>
     );
   }
@@ -32,10 +70,12 @@ class ProjectBoard extends Component {
 ProjectBoard.propTypes = {
   getBacklog: PropTypes.func.isRequired,
   backlog: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   backlog: state.backlog,
+  errors: state.errors,
 });
 
 export default connect(mapStateToProps, { getBacklog })(ProjectBoard);
