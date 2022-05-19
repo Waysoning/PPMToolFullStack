26544781f6694faddeec3e60c1e6e51aa13b2ccb
wayson <wayson1225@outlook.com>
2022-05-19 11:55:13 -0700

    create user object, validation, repository, service

--- PPMTool-SpringBoot-Server/src/main/java/io/waysoning/ppmtool/domain/User.java
+++ PPMTool-SpringBoot-Server/src/main/java/io/waysoning/ppmtool/domain/User.java
@@ -1,4 +1,97 @@
 package io.waysoning.ppmtool.domain;
 
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email(message = "Username needs to be an email")
+    @NotBlank(message = "Username is required")
+    @Column(unique = true)
+    private String username;
+    @NotBlank(message = "Please enter your full name")
+    private String fullName;
+    @NotBlank(message = "Password is required")
+    private String password;
+    @Transient
+    private String confirmPassword;
+    private Date createAt;
+    private Date updateAt;
+
+    // OneToMany with Project
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+
+    public Date getUpdateAt() {
+        return updateAt;
+    }
+
+    public void setUpdateAt(Date updateAt) {
+        this.updateAt = updateAt;
+    }
+
+    @PrePersist
+    protected void onCreate(){
+        this.createAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updateAt = new Date();
+    }
 }
---
+++ PPMTool-SpringBoot-Server/src/main/java/io/waysoning/ppmtool/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package io.waysoning.ppmtool.repositories;
+
+import io.waysoning.ppmtool.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+//    User findByUsername(String username);
+}
+
