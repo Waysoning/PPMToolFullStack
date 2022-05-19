4cb99a36fa92d3b5f773d2df3abc29ba57bd285c
wayson <wayson1225@outlook.com>
2022-05-19 12:31:19 -0700

    user registration part 1

--- PPMTool-SpringBoot-Server/src/main/java/io/waysoning/ppmtool/domain/User.java
+++ PPMTool-SpringBoot-Server/src/main/java/io/waysoning/ppmtool/domain/User.java
@@ -1,12 +1,17 @@
 package io.waysoning.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import javax.persistence.*;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import java.util.Collection;
 import java.util.Date;
 
 @Entity
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -93,5 +98,35 @@ public class User {
     @PreUpdate
     protected void onUpdate(){
         this.updateAt = new Date();
+    }
+
+    @Override
+    @JsonIgnore
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    @JsonIgnore
+    public boolean isEnabled() {
+        return true;
     }
 }
--- PPMTool-SpringBoot-Server/src/main/java/io/waysoning/ppmtool/services/UserService.java
+++ PPMTool-SpringBoot-Server/src/main/java/io/waysoning/ppmtool/services/UserService.java
@@ -1,7 +1,9 @@
 package io.waysoning.ppmtool.services;
 
+import io.waysoning.ppmtool.domain.User;
 import io.waysoning.ppmtool.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +11,12 @@ public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public User saveUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
 }
